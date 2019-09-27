32bb2d3c15f1062dddbd619bc63598b083d7b271
Abhinav Sagar <40603139+abhinavsagar@users.noreply.github.com>
2019-09-27 16:36:35 +0530

    Update run.py

--- neural network/run.py
+++ neural network/run.py
@@ -1,14 +1,11 @@
 import numpy as np
-
 from nn import NeuralNetwork
-
 from sklearn import datasets
 from sklearn import preprocessing
 from sklearn import model_selection
 from sklearn import metrics
 
 def targetToVector(x):
-	# Vector
 	a = np.zeros([len(x),10])
 	for i in range(0,len(x)):
 		a[i,x[i]] = 1
@@ -16,22 +13,17 @@ def targetToVector(x):
 
 if __name__ == '__main__':
 
-	# Digits dataset loading
 	digits = datasets.load_digits()
 	X = preprocessing.scale(digits.data.astype(float))
 	y = targetToVector(digits.target)
 
-	# Cross valitation
 	X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=0)
 	
-	# Neural Network initialization
 	NN = NeuralNetwork(64,60,10, output_act = 'softmax')
 	NN.fit(X_train,y_train, epochs = 50, learning_rate = .1, learning_rate_decay = .01, verbose = 1)
 
-	# NN predictions
 	y_predicted = NN.predict(X_test)
 
-	# Metrics
 	y_predicted = np.argmax(y_predicted, axis=1).astype(int)
 	y_test = np.argmax(y_test, axis=1).astype(int)
 
